nta/ytm-listener

commit 8f03853e4dc19526be53fe4f8f94fe1032c744b3

fix: only add artists with id to 'artistNames'
another fix for OMV/UGC

app.py
@@ -104,7 +104,10 @@ class Listener:
                 artists = []
 
                 for artist in cur_track['artists']:
-                    artists.append(artist['name'])
+                    # OMVs and UGCs tend to have artists along the lines of [{id: "UC2cUfTntum9HJa0kkG0butQ", name: "SAWTOWNE"}, {id: None, name: "583K views"}]
+                    # 'SAWTOWNE & 583K views' is a *horrible* artist name
+                    if artist['id']:
+                        artists.append(artist['name'])
 
                 cur_track['artistNames'] = ' & '.join(artists)
 

commit ead0759ae2f10eada1cb8475714b15f3a931bbb1

fix: empty artists
regression in 8f03853e4dc19526be53fe4f8f94fe1032c744b3, 'tseems some tracks just have null-y artists

app.py
@@ -106,7 +106,10 @@ class Listener:
                 for artist in cur_track['artists']:
                     # OMVs and UGCs tend to have artists along the lines of [{id: "UC2cUfTntum9HJa0kkG0butQ", name: "SAWTOWNE"}, {id: None, name: "583K views"}]
                     # 'SAWTOWNE & 583K views' is a *horrible* artist name
-                    if artist['id']:
+                    #
+                    # except tracks like https://music.youtube.com/watch?v=scx4MSegXZI have, uh, "artists":[{"id":null,"name":"SEGA & Keitarou Hanada"}], so also add if this is an official
+                    # music upload as in those cases we *probably* won't have views
+                    if artist['id'] or ('videoType' in cur_track and cur_track['videoType'] == 'MUSIC_VIDEO_TYPE_ATV'):
                         artists.append(artist['name'])
 
                 cur_track['artistNames'] = ' & '.join(artists)